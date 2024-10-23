1b182cde036feb55f6000d2469279ed68fc11ddc
canavan-a <aidan.canavan3@gmail.com>
2024-10-23 16:42:56 -0400

    added env accessors andcatch all for pre deployment

--- client/src/context/GlobalContext.jsx
+++ client/src/context/GlobalContext.jsx
@@ -1,14 +1,19 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 const GlobalContext = createContext();
 
 export const GlobalContextProvider = ({ children }) => {
+  const envUrl = import.meta.env.VITE_APP_SERVER_URI;
   const [server, setServer] = useState(
-    `${import.meta.env.VITE_APP_SERVER_URI}`
+    envUrl.includes("localhost") ? envUrl : ""
   );
 
+  const envWsUrl = import.meta.env.VITE_APP_WS_ENDPOINT;
+
   const [websocketServer, setWebsocketServer] = useState(
-    `${import.meta.env.VITE_APP_WS_ENDPOINT}`
+    envWsUrl.includes("localhost")
+      ? envWsUrl
+      : `wss://${window.location.hostname}`
   );
 
   const contextValue = {
--- server/main.go
+++ server/main.go
@@ -4,24 +4,49 @@ import (
 	"fmt"
 	"main/sweeper"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
+
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+
+	// Configure CORS
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"}, // Change this to match your frontend URL
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		AllowCredentials: true,
+	}))
+
 	r.SetTrustedProxies(nil)
 
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{"response": "server is running"})
-	})
+	r.LoadHTMLGlob("dist/*.html")
+
+	r.Static("/assets", "./assets")
+
+	// r.GET("/", func(c *gin.Context) {
+	// 	c.JSON(200, gin.H{"response": "server is running"})
+	// })
 
 	serverG := sweeper.InitServerGames()
 
 	r.GET("/publish", serverG.PublisherRoute)
 	r.GET("/subscribe", serverG.SubscriberRoute)
 	r.GET("/menu", serverG.MenuConnectionRoute)
+
+	r.NoRoute(func(c *gin.Context) {
+		fmt.Println("you are in no route")
+		c.HTML(200, "index.html", nil)
+	})
 
 	fmt.Println("server has started")
 	r.Run(":80")
